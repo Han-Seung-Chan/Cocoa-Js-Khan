7be282add465ac362d8e6231481e9b1404b0e15c
seungchanHan <seungchanhan00@gmail.com>
2021-11-04 00:29:06 +0900

    add console result

--- Day2/binary.js
+++ Day2/binary.js
@@ -1,5 +1,5 @@
 // solution(2, 4, 2); //2진수, 4개의 숫자까지, 2명이 말할때
-// ["0", "1", "1", "0", "1", "1", "1", "0", "0", "1", "0", "1", "1", "1", "0", "1", "1", "1"]
+// ['0', '1', '1', '0', '1', '1', '1', '0', '0', '1', '0', '1', '1', '1', '0', '1', '1', '1']
 function solution(binary, number, player) {
   let answer = [];
   for (let value = 0; value < number * player; value++) {
@@ -8,4 +8,12 @@ function solution(binary, number, player) {
   }
   console.log(answer);
 }
-solution(2, 4, 2);
+solution(10, 5, 3);
+/* 콘솔 결과
+[
+  '0', '1', '1', '0', '1',
+  '1', '1', '0', '0', '1',
+  '0', '1', '1', '1', '0',
+  '1', '1', '1'
+]
+*/
