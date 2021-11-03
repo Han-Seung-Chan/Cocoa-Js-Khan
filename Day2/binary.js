e2fe6156153251df42e306b2747bafb6e81e320a
seungchanHan <seungchanhan00@gmail.com>
2021-11-03 16:18:52 +0900

    birnary

---
+++ Day2/binary.js
@@ -0,0 +1,11 @@
+// solution(2, 4, 2); //2진수, 4개의 숫자까지, 2명이 말할때
+// ["0", "1", "1", "0", "1", "1", "1", "0", "0", "1", "0", "1", "1", "1", "0", "1", "1", "1"]
+function solution(binary, number, player) {
+  let answer = [];
+  for (let value = 0; value < number * player; value++) {
+    let change = value.toString(binary).split('');
+    answer.push(...change);
+  }
+  console.log(answer);
+}
+solution(2, 4, 2);
