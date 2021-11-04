9ed279e9d238603f81d258d37c70885cfd5e38f8
seungchanHan <seungchanhan00@gmail.com>
2021-11-04 14:07:14 +0900

    modify value

--- Day2/array/factorial.js
+++ Day2/array/factorial.js
@@ -2,10 +2,12 @@
 function multiply(number) {
   let factorial = [1];
 
-  for (let i = 0; i <= number; i++) {
+  for (let i = 2; i <= number; i++) {
     factorial.push(factorial[factorial.length - 1] * i);
   }
   return factorial;
 }
-
 console.log(multiply(4));
+/* 콘솔 결과
+[ 1, 2, 6, 24 ]
+*/
