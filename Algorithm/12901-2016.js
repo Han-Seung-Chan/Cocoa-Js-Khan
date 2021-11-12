af2202637162d50b0334fcd20bbe5758a681b54b
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2021-11-12 23:13:48 +0900

    Delete 12901-2016.js

--- Algorithm/12901-2016.js
+++
@@ -1,7 +0,0 @@
-function solution(a, b) {
-  const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-  let result = days[new Date(2016, a - 1, b).getDay()];
-
-  return result;
-}
-console.log(solution(5, 24));
